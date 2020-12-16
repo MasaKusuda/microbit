63769a04670a42c14314b0066442608d4df5254b
MasaKusuda <masa.kusuda.1999@gmail.com>
2020-12-16 14:01:21 +0900

    implement some func

--- sound.js
+++ sound.js
@@ -6,7 +6,7 @@ bufr.setNumber(NumberFormat.UInt8BE, 2, 0x02);
 bufr.setNumber(NumberFormat.UInt8BE, 3, 0x00);
 bufr.setNumber(NumberFormat.UInt8BE, 4, Track);
 bufr.setNumber(NumberFormat.UInt8BE, 5, Track+0xB3);
-serial.writeBuffer(bufr)
+serial.writeBuffer(bufr);
 }
 function volume(vol:number){
     let bufr = pins.createBuffer(5);
@@ -15,10 +15,27 @@ function volume(vol:number){
     bufr.setNumber(NumberFormat.UInt8BE, 2, 0x01);
     bufr.setNumber(NumberFormat.UInt8BE, 3, vol);
     bufr.setNumber(NumberFormat.UInt8BE, 4, vol+0xBE);
-serial.writeBuffer(bufr)
-
+serial.writeBuffer(bufr);
 }
-
+function set_the_loop_mode(mode:number){
+    let bufr = pins.createBuffer(5);
+    bufr.setNumber(NumberFormat.UInt8BE, 0, 0xAA);
+    bufr.setNumber(NumberFormat.UInt8BE, 1, 0x18);
+    bufr.setNumber(NumberFormat.UInt8BE, 2, 0x01);
+    bufr.setNumber(NumberFormat.UInt8BE, 3, mode);
+    bufr.setNumber(NumberFormat.UInt8BE, 4, mode+0xC3);
+serial.writeBuffer(bufr);
+}
+function set_the_numbers_of_loop_time(times:number){
+    let bufr = pins.createBuffer(5);
+    bufr.setNumber(NumberFormat.UInt8BE, 0, 0xAA);
+    bufr.setNumber(NumberFormat.UInt8BE, 1, 0x19);
+    bufr.setNumber(NumberFormat.UInt8BE, 2, 0x02);
+    bufr.setNumber(NumberFormat.UInt8BE, 3, 0x00);
+    bufr.setNumber(NumberFormat.UInt8BE, 4, times);
+    bufr.setNumber(NumberFormat.UInt8BE, 4, times+0xC5);
+serial.writeBuffer(bufr);
+}
 
 let i:number = 0
 serial.redirect(
@@ -28,9 +45,11 @@ BaudRate.BaudRate9600
 )
 volume(20);
 basic.forever(function () {
+    let i:number = 0;
     while (true) {
-        play(17)
-        basic.pause(1000)
+        play(i)
+        basic.pause(10000)
         led.toggle(0, 0)
+        i++;
     }
 })
