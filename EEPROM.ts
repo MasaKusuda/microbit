bdcab31c9ac1638da0a405ee88fcc75f33464eee
MasaKusuda <masa.kusuda.1999@gmail.com>
2021-01-08 17:46:58 +0900

    made setEEPBigNum

--- EEPROM.ts
+++ EEPROM.ts
@@ -11,46 +11,53 @@ namespace EEPROM{
     let DS1307_REG_RAM = 8
 
     function setReg(reg: number, dat: number): void {
-        let buf = pins.createBuffer(2);
-        buf[0] = reg;
-        buf[1] = dat;
-        pins.i2cWriteBuffer(DS1307_I2C_ADDR, buf);
+    let buf = pins.createBuffer(2);
+    buf[0] = reg;
+    buf[1] = dat;
+    pins.i2cWriteBuffer(DS1307_I2C_ADDR, buf);
     }
 
-        function getReg(reg: number): number {
-        pins.i2cWriteNumber(DS1307_I2C_ADDR, reg, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(DS1307_I2C_ADDR, NumberFormat.UInt8BE);
-        }
+    function getReg(reg: number): number {
+    pins.i2cWriteNumber(DS1307_I2C_ADDR, reg, NumberFormat.UInt8BE);
+    return pins.i2cReadNumber(DS1307_I2C_ADDR, NumberFormat.UInt8BE);
+    }
     
-        function HexToDec(dat: number): number {
-        return (dat >> 4) * 10 + (dat % 16);
-        }
+    function HexToDec(dat: number): number {
+    return (dat >> 4) * 10 + (dat % 16);
+    }
     
-        function DecToHex(dat: number): number {
-        return Math.idiv(dat, 10) * 16 + (dat % 10)
-        }
+    function DecToHex(dat: number): number {
+    return Math.idiv(dat, 10) * 16 + (dat % 10)
+    }
     
     export function setEEPRom(data: number): void{
-        setReg(DS1307_REG_RAM, DecToHex(data % 100))
-    }
-    export function getEEPROM(): number{
-        return HexToDec(getReg(DS1307_REG_RAM))
+    setReg(DS1307_REG_RAM, DecToHex(data % 100))
     }
 
-    
-    export function setEEPBigNum(data: number): void{
-        let buf = pins.createBuffer(3);
-        buf[0] = DS1307_REG_RAM;
-        buf[1] = DecToHex(data % 255);
-        if (data - 255 >= 0) {
-            buf[2] = DecToHex(data - 255);
-        } else {
-            buf[2] = 0;
+    export function getEEPROM(): number{
+    return HexToDec(getReg(DS1307_REG_RAM))
+    }
+
+    export function setRegBig(reg: number, dat:Buffer): void {
+        let length: number = dat.length;
+        let buf = pins.createBuffer(length + 1);
+        buf[0] = reg;
+        for (let i = 1; i <= length;i++){
+            buf[i]=dat[i];
         }
         pins.i2cWriteBuffer(DS1307_I2C_ADDR, buf);
     }
+
+    export function setEEPBigNum(data:Buffer): void{
+        let buf:Buffer = pins.createBuffer(data.length);
+        for(let i=0;i<data.length;i++){
+            buf[i]=DecToHex(data[i]%100);
+        }
+        setRegBig(DS1307_REG_RAM,buf);
+    }
+
     export function getEEPBigNum(): number{
-        pins.i2cWriteNumber(DS1307_I2C_ADDR, DS1307_REG_RAM, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(DS1307_I2C_ADDR, NumberFormat.UInt8BE);
+    pins.i2cWriteNumber(DS1307_I2C_ADDR, DS1307_REG_RAM, NumberFormat.UInt8BE);
+    return pins.i2cReadNumber(DS1307_I2C_ADDR, NumberFormat.UInt8BE);
     }
-    }
+}
